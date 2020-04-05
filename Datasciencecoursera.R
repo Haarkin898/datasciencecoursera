520bde89284a3bc8017dba3bff823d9a7c5a1c80
Haarkin898 <akinola.ibrahim@gmail.com>
2020-04-05 18:59:06 +0200

    Initial commit of this very particular file

---
+++ Datasciencecoursera.R
@@ -0,0 +1,7 @@
+print ("This file was crated for the final Quiz on RStudio")
+
+print("To be precise, it is for the 1st course in the coursera Data Science specialization")
+
+print("it will be reviewed by peers and use in grading the author")
+
+print("And now it is stored on GitHub")
